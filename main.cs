5121389ddfb3dee38b792ba858cb33fb8e4fbf75
1473883 <116083770+1473883@users.noreply.github.com>
2022-10-18 15:18:35 +0000

    Added Truncate and Name

--- main.cs
+++ main.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 class Program {
-  public static void Main()
+  public static void Main(string[]args)
     {
       //create array for months of the year
       string[] months = new string[12] 
@@ -18,6 +18,7 @@ class Program {
 
       //output the random month name 
       Console.WriteLine("Month is {0}", months[month-1]);
+      
 
       //create array for day in the momth
       string[] days = new string[30]
@@ -33,6 +34,9 @@ class Program {
       //output the random day number
       Console.WriteLine("Day is {0}", days[day-1]);
       RangeTest();
+      TruncateTest();
+      GetLengthOfString("Joshua Bell");
+      GetCharOfString("Joshua Bell", 4);
     }
 
   public static void RangeTest() {
@@ -57,4 +61,78 @@ class Program {
      100
      */
   }
+  public static void RoundTest()
+    {
+        float originalNumber1 = 1.23f;
+        float originalNumber2 = 3.15f;
+        float originalNumber3 = 1.9f;
+
+ 
+
+        float roundedNumber1 = (float)Math.Ceiling(originalNumber1);
+        float roundedNumber2 = (float)Math.Round(originalNumber2, 1);
+        float roundedNumber3 = (float)Math.Floor(originalNumber3);
+
+ 
+
+        Console.WriteLine("roundedNumber1 is " + roundedNumber1);
+        Console.WriteLine("roundedNumber2 is " + roundedNumber2);
+        Console.WriteLine("roundedNumber3 is " + roundedNumber3);
+    }
+
+ 
+
+    public static void TruncateTest()
+    {
+
+ 
+
+        decimal decimalNumber;
+
+ 
+
+        decimalNumber = 32.7865m;
+
+ 
+
+        Console.Write(Math.Truncate(decimalNumber));
+
+ 
+
+        decimalNumber = -32.9012m;
+
+ 
+
+        Console.WriteLine(Math.Truncate(decimalNumber));
+
+ 
+
+    }
+
+ 
+
+    public static void GetLengthOfString(string str)
+    {
+        int length;
+
+ 
+
+        length = str.Length;
+        Console.WriteLine("{0} is {1} characters long", str, length);
+    }
+
+ 
+
+    public static void GetCharOfString(string str, int i)
+    {
+        char singleLetter;
+
+ 
+
+        singleLetter = str[i];
+        Console.WriteLine("Character {0} of {1} is {2}", i, str, singleLetter);
+
+ 
+
+    }
 }
